17ce48c1f15240e92db64e6340f8eb16943b0474
saul <luckly083@gmail.com>
2023-12-27 12:32:45 -0500

    feat: adding basic routes and backend

--- get-aboard-next/src/app/demo/page.tsx
+++ get-aboard-next/src/app/demo/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home Page
+      demo
     </main>
   );
 }
