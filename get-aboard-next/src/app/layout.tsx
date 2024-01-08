780c91761bf6a1ebb37a9cd63c87292930ffe0d9
saul <luckly083@gmail.com>
2024-01-07 21:59:05 -0500

    feat: adding vercel analytics

--- get-aboard-next/src/app/layout.tsx
+++ get-aboard-next/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import NavBar from "@/components/Navigation/NavBar";
 import Providers, { ThemeProvider } from "./providers";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -30,6 +31,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
